fbd312696e8f5c46f9a28c205d353ba2cf9ca847
lightflix <lightflix@gmail.com>
2022-05-07 23:47:38 +0300

    Bug fixes, added color coding

--- firevue.py
+++ firevue.py
@@ -2,6 +2,13 @@
 import json
 import glob
 
+class color:
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   BOLD = '\033[1m'
+   END = '\033[0m'
+
 def keyCheck(object, key):
 
     #the object should be a dict, not an string. It could be a string if the keyCheck() returned "[not available]" on a recursive call.
@@ -15,6 +22,8 @@ def keyCheck(object, key):
         #if if value from key is an int, then we convert it to str on return.
         if type(value) == int:
             return str(value)
+        elif value is None:
+            return "[not available]"
         return value
 
 def decoder(ciphertext):
@@ -47,14 +56,13 @@ def harParse():
     har_file = glob.glob('*.har') 
 
     if len(har_file) != 1:
-        raise ValueError("[x] Error: There must only be one har file in the current directory.")
+        raise ValueError("[x] Error: HAR file does not exist in current directory or there is more than one HAR file in current directory!")
 
     #pick the only har file in current directory. 
     har_filename = har_file[0]
 
     #load json from the HAR file
     har_file = json.load(open(har_filename, encoding="utf-8"))
-
     entries = keyCheck(keyCheck(har_file, 'log'), 'entries')
 
     #iterate though each request made by the browser 
@@ -75,23 +83,24 @@ def harParse():
 def infoGet(content):
 
     question_list = keyCheck(content,'questions')
-    print("\n[-] Interview Details\n")
+    print(color.BOLD+"\n[-] Interview Details\n"+color.END)
 
-    print("  - Interviewer: "+keyCheck(content,'interviewer'))
-    print("  - Role: "+keyCheck(content,'position'))
-    print("  - Number of questions (includes games section): "+keyCheck(content,'questionCount'))
-    print("  - Number of retries allowed: "+keyCheck(content,'retryAllowance'))
-    print("  - Interview Duration: "+keyCheck(content,'interviewDurationMinutes')+" mins")
-    print("  - Invite Date: "+keyCheck(keyCheck(content, 'interviewUses')[0], 'invitedDate'))
+    print(color.GREEN+"  - Interviewer: "+color.END+keyCheck(content,'interviewer'))
+    print(color.GREEN+"  - Role: "+color.END+keyCheck(content,'position'))
+    print(color.GREEN+"  - Number of questions (includes games section): "+color.END+keyCheck(content,'questionCount'))
+    print(color.GREEN+"  - Number of retries allowed: "+color.END+keyCheck(content,'retryAllowance'))
+    print(color.GREEN+"  - Interview Duration: "+color.END+keyCheck(content,'interviewDurationMinutes')+" mins")
+    print(color.GREEN+"  - Estimated Duration: "+color.END+keyCheck(content,'estimatedMinutesToComplete')+" mins")
+    print(color.GREEN+"  - Invite Date: "+color.END+keyCheck(keyCheck(content, 'interviewUses')[0], 'invitedDate'))
 
-    print("\n[-] Interview Questions")
+    print(color.BOLD+"\n[-] Interview Questions"+color.END)
     if question_list == "[not available]":
         print("[x] Not available")
     else:
         i = 0
         for q in question_list:
             if keyCheck(q,'type') != "mindx-assessment":
-                print("\n  - Question "+str(i+1)+": Prep Time (sec): "+keyCheck(q,'prepTimeSeconds')+", Max Answer Duration (sec): "+keyCheck(q,'maxDuration'))
+                print(color.GREEN+"\n  - Question "+str(i+1)+": Prep Time (sec): "+keyCheck(q,'prepTimeSeconds')+", Max Answer Duration (sec): "+keyCheck(q,'maxDuration')+", Answer type: "+keyCheck(q,'type')+color.END+"\n")
 
                 # send each question into the decoder that converts it back to plaintext. 
                 print(decoder(keyCheck(q,'text')))
@@ -101,14 +110,18 @@ def infoGet(content):
 if __name__ == "__main__":
 
     #parse result contains the interview details and all questions.
-    parse_result = harParse()
+    try:
+        parse_result = harParse()
+    except ValueError as e:
+        print(str(e))
+        exit()
 
     if parse_result == False:
         print("\n[x] ERROR: Incorrect HAR parsing, exiting.")
         exit()
 
-    print("\n--== Infinite Prep Time: A Hirevue Question Cracker v1.0 ==--")
+    print(color.YELLOW+"\n--== Infinite Prep Time: A Hirevue Question Cracker v1.0 ==--"+color.END)
 
     #fetch values and print them neatly
     infoGet(parse_result)
-    print("\n[-] Done!\n")
+    print(color.BLUE+"\n[-] Good luck! :D\n"+color.END)
